2b37c32163b5506e41e7d9751b8babb879f59be1
jamesrusso12 <124114717+jamesrusso12@users.noreply.github.com>
2024-10-01 17:45:42 -0600

    update

---
+++ bootstrap/js/portfolio.js
@@ -0,0 +1,39 @@
+$(function(){
+
+    // to load preview on click
+    var selector = $('.portfolio-item__wrapper');
+    if(selector.length>0){
+      selector.click(function(){
+        var titleDiv = $('#iframe-content .demo-title');
+        var demo_url = $(this).data('demo');
+        var demo_title = $(this).data('title');
+        titleDiv.text(demo_title);
+        var html = '';
+        html += '<div class="iframe-wrapper"><iframe id="demo-iframe" src="'+ demo_url +'">This feature requires inline frames. You have iframes disabled or your browser does not support them.</iframe></div>';
+        $('.devices button[data-device-mode="desktop"]').addClass('active');
+        $('#iframe-content').append(html).show();
+      });
+    }
+  
+    // to close preview 
+    var closePreview = $('.preview-close');
+    if(closePreview.length > 0){
+      closePreview.click(function(){
+        $('#iframe-content').hide().find('.iframe-wrapper').remove();
+        $('.devices button').removeClass('active');
+        $('#iframe-content').attr('class', '');
+      });
+    }
+  
+    //For responsiveness tab
+    var responsiveTabs = $('.devices button[data-device-mode]');
+    if(closePreview.length > 0){
+       responsiveTabs.click(function(){
+         responsiveTabs.removeClass('active');
+         $(this).addClass('active');
+         var deviceMode = $(this).data('device-mode');
+         var targetIframe = $('#iframe-content');
+         targetIframe.attr('class', deviceMode);
+       });
+    }
+  });
---
+++ bootstrap/js/toggleReadMore.js
@@ -0,0 +1,15 @@
+function toggleReadMore(button) {
+    var parent = button.previousElementSibling;
+    var dots = parent.querySelector(".dots");
+    var moreText = parent.querySelector(".more-content");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        button.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        button.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
