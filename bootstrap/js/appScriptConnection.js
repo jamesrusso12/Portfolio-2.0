12c8856ab393af2f39e3bd53f785ec8abd3ff006
jamesrusso12 <jrusso03@icloud.com>
2024-10-15 23:49:46 -0600

    Contact Me fixes

---
+++ bootstrap/js/appScriptConnection.js
@@ -0,0 +1,20 @@
+//JavaScript Code for Form Submission
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxiqg9ZZH_pLkSR8T-Dm3xc5bQ3lrGRJU0oUSl9uEg5LpT1Dbpryv3bVF5Vr-G4OJKG/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully!";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            msg.innerHTML = "Failed to send message!";
+        });
+});
