c07fa5e38e02775337423d4ea884b290abb6e3b4
jamesrusso12 <124114717+jamesrusso12@users.noreply.github.com>
2025-05-14 18:26:04 -0600

    Fixed the video section fully pt 1

--- bootstrap/js/portfolio.js
+++ bootstrap/js/portfolio.js
@@ -5,12 +5,6 @@ jQuery(function ($) {
         margin: 10,
         nav: true,
         items: 1,
-        dots: false,
-        stagePadding: 120,     // shows ~120px of each side slide
-        responsive: {
-            0: { stagePadding: 30 },
-            600: { stagePadding: 60 },
-            1000: { stagePadding: 120 }
-        }
+        dots: false
     });
 });
